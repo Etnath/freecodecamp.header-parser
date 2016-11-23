763c4d78fe57cadcc7943f8919aa4acf9e11f403
Kevin Giffone <kevin.giffone@gmail.com>
2016-11-23 15:11:51 +0000

    solution

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 var express = require('express')
-var app = express()
 var port = process.env.PORT || 8080
+
+var app = express()
 
 app.get('/', function (req, res) 
 {
-  res.send('Hello World!')
+    var ip = req.get('x-forwarded-for')
+    var language = req.get('accept-language')
+    var software = req.get('user-agent')
+    
+    if(language.split(',').length > 1)
+    {
+        language = language.split(',')[0]
+    }
+    
+    software = software.split('(')[1]
+    software = software.split(')')[0]
+    
+    var object = {
+        "ipaddress": ip,
+        "language": language,
+        "software": software
+    }
+    
+    res.send(JSON.stringify(object))
 })
 
 app.listen(port, function () 
 {
-  console.log('Example app listening on port ' + port + '!');
+  console.log('Example app listening on port ' + port + '!')
 })
